a5a050988bdb5c0fac61ce8d828628905779ec61
myyusuf <myyusuf1911@gmail.com>
2017-06-21 11:34:07 +0700

    Release 2 timeline

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const OAuth = require('oauth');
+const dotenv = require('dotenv').config();
 
 const index = require('./routes/index');
 
@@ -9,5 +11,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/tweets', index);
+
+const oauth = new OAuth.OAuth(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/access_token',
+    process.env.API_KEY,
+    process.env.API_SECRET,
+    '1.0A',
+    null,
+    'HMAC-SHA1'
+  );
+
+app.locals.oauth = oauth;
 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,24 @@
 const express = require('express');
-const OAuth = require('oauth');
 
 const router = express.Router();
 const dotenv = require('dotenv').config();
 
 router.get('/test', (req, res) => {
-
-  const oauth = new OAuth.OAuth(
-      'https://api.twitter.com/oauth/request_token',
-      'https://api.twitter.com/oauth/access_token',
-      process.env.API_KEY,
-      process.env.API_SECRET,
-      '1.0A',
-      null,
-      'HMAC-SHA1'
-    );
-  oauth.get(
+  req.app.locals.oauth.get(
     'https://api.twitter.com/1.1/trends/place.json?id=23424977',
+    process.env.TOKEN,
+    process.env.TOKEN_SECRET,
+    function (e, data, resp) {
+      if (e) console.error(e);
+      console.log(data);
+      res.send(data);
+    });
+});
+
+router.get('/timelines', function(req, res) {
+  const url =  'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=Yusuf_hacktiv8&count=2';
+  req.app.locals.oauth.get(
+    url,
     process.env.TOKEN,
     process.env.TOKEN_SECRET,
     function (e, data, resp) {
